c355be9711cb113849b1844a379e337c15bc497e
Caren Wanjiru Mwaka Chibwara <carrienjiru40@gmail.com>
2024-03-29 08:32:38 +0300

    modifies index.js

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Code your solution here
+// const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+}
+
+console.log((findMatching(drivers, 'Bobby')));  
+console.log((findMatching(drivers, 'Sammy'))); 
+
+
+function fuzzyMatch(drivers, letters) {
+    return drivers.filter(driver => driver.startsWith(letters));
+    }
+    console.log(fuzzyMatch(drivers, 'Sa'))
+
+    const drivers = [
+        {
+          name: 'Bobby',
+          hometown: 'Pittsburgh'
+        },
+        {
+          name: 'Sammy',
+          hometown: 'New York'
+        },
+        {
+          name: 'Sally',
+          hometown: 'Cleveland'
+        },
+        {
+          name: 'Annette',
+          hometown: 'Los Angeles'
+        },
+        {
+          name: 'Bobby',
+          hometown: 'Tampa Bay'
+        }
+      ];
+
+function matchName(drivers, name){
+    return drivers.filter(driver => driver.name === name)
+}
+console.log(matchName(drivers, 'Bobby'))
